dd0aa2af8c77df3a30c75813627fb17bb7d9c490
NguyenToan234 <nguyenvantoanpr359@gmail.com>
2026-03-30 22:02:02 +0700

    AddIMGInCart

--- EcommerceTeaShop.Common/DTOs/CartDTO.cs
+++ EcommerceTeaShop.Common/DTOs/CartDTO.cs
@@ -35,5 +35,7 @@ namespace EcommerceTeaShop.Common.DTOs
         public decimal Price { get; set; }
 
         public int Quantity { get; set; }
+        public string Image { get; set; } // ✅ thêm dòng này
+
     }
 }
